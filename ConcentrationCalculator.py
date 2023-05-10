d3110049d91a27c34d6008d3e744c107e262d14a
Karol Pępiak <115930524+karolpepiak@users.noreply.github.com>
2023-05-10 16:27:17 +0200

    ConcentrationCalculator.py

--- ConcentrationCalculator.py
+++ ConcentrationCalculator.py
@@ -1,5 +1,9 @@
 #Concentration calculator - env (Python 3.11 (64-bit)) by Karol Pepiak
 from queue import Empty
+import os
+
+def clear():
+    os.system('cls')
 
 def find_percentage_concentration(solvent_mass, substance_mass):
     solution_mass = solvent_mass + substance_mass
@@ -15,21 +19,25 @@ app_running = True
 user_choise = 0
 temp_input_string = str(Empty)
 temp_bool = True
+history_array = []
 
 print("This application was created to calculate concentrations of chemical substances.")
 
 while app_running is True:
 
-    print("Choose what You want to calculate:")
-    print("[1] Percentage concentration")
-    print("[2] Molar concentration")
+    os.system('cls')
+    print("Choose what You want to do:")
+    print("[1] Calculate percentage concentration")
+    print("[2] Calculate molar concentration")
+    print("[3] Show history")
 
     while temp_bool is True:
         try:
             user_choise = int(input())
-            if user_choise != 1 and user_choise != 2:
-                print("Type 1 or 2")
+            if user_choise != 1 and user_choise != 2 and user_choise !=3:
+                print("Type number from 1 to 3")
             else:
+                clear()
                 temp_bool = False
         except:
             print("Wrong input format!")
@@ -43,6 +51,7 @@ while app_running is True:
             try:
                 substance_mass = float(input())
                 temp_bool = False
+                clear()
             except:
                 print("Wrong input format!")
                 substance_mass = 0
@@ -54,6 +63,7 @@ while app_running is True:
             try:
                 solvent_mass = float(input())
                 temp_bool = False
+                clear()
             except:
                 print("Wrong input format!")
                 solvent_mass = 0
@@ -61,8 +71,10 @@ while app_running is True:
         temp_bool = True
 
         result = find_percentage_concentration(solvent_mass, substance_mass)
-        print("The percentage concentration of " + str(substance_mass) + "g of substance dissolved in " + str(solvent_mass) + "g of solvent is: " + str(result) + "%.")
-        
+        msg = "The percentage concentration of " + str(substance_mass) + "g of substance dissolved in " + str(solvent_mass) + "g of solvent is: " + str(result) + "%."
+        history_array.append(msg)
+        print(msg)
+
     elif user_choise == 2:
         
         print("Enter substance mass [g]:")
@@ -70,6 +82,7 @@ while app_running is True:
             try:
                 substance_mass = float(input())
                 temp_bool = False
+                clear()
             except:
                 print("Wrong input format!")
                 substance_mass = 0
@@ -81,6 +94,7 @@ while app_running is True:
             try:
                 molar_mass = float(input())
                 temp_bool = False
+                clear()
             except:
                 print("Wrong input format!")
                 #molar mass for water
@@ -93,6 +107,7 @@ while app_running is True:
             try:
                 volume = float(input())
                 temp_bool = False
+                clear()
             except:
                 print("Wrong input format!")
                 volume = 0
@@ -100,8 +115,17 @@ while app_running is True:
         temp_bool = True
 
         result = find_molar_concentration(substance_mass, molar_mass, volume)
-        print("The molar concentration of " + str(substance_mass) + "g of substance with molar mass equal to " + str(molar_mass) + "g/mol dissolved in " + str(volume) + "dm3 of solvent is: " + str(result) + "mol/dm3.")
+        msg = "The molar concentration of " + str(substance_mass) + "g of substance with molar mass equal to " + str(molar_mass) + "g/mol dissolved in " + str(volume) + "dm3 of solvent is: " + str(result) + "mol/dm3."
+        history_array.append(msg)
+        print(msg)
     
+    elif user_choise == 3:
+        clear()
+        for i in range(len(history_array)):
+            element = history_array[i]
+            print("[" + str(i) + "] " + str(element))
+
+
     print("Do You want to calculate another concentration?")
     print("[Y] Yes")
     print("[N] No")
@@ -124,16 +148,3 @@ while app_running is True:
             temp_input_string = str(Empty)
 
     temp_bool = True
-
-
-
-
-
-
-    
-
-
-
-
-
-
